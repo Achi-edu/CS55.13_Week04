6319bc7a2e8ef93276da290cacc1b204a6e98bd8
Achilles Poloynis <achilles@achillespoloynis.com>
2024-12-20 14:02:15 -0800

    add routing, styles, and fixed linting issues

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,19 @@
-import "@/styles/globals.css";
+import { useEffect, useState } from "react";
+import "@/styles/global.scss";
+import Layout from '@/components/Layout';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+    const [theme, setTheme] = useState("light");
+
+    useEffect(() => {
+        const storedTheme = localStorage.getItem("theme") || "light";
+        setTheme(storedTheme);
+        document.documentElement.setAttribute("data-theme", storedTheme);
+    }, []);
+
+  return (
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+  );
 }
